0ce375f91faaac149ff04dc6470aa8988874a02c
Pakkoyan <maksimenkobogdan12@gmail.com>
2023-10-20 20:10:14 +0600

    FirstAndLast2

--- MyClassLib/WorldOfTanks/Tank.cs
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -1,6 +1,65 @@
+using System.Reflection;
+
 namespace MyClassLib.WorldOfTanks;
 
-public class Tank
+public abstract class Tank
 {
+    readonly int Ammunition;
+    readonly int LevelOfArmor;
+    readonly int Maneuverability;
+
+    public Tank()
+    {
+        Random random = new Random();
+        Ammunition = random.Next(1, 101);
+        LevelOfArmor = random.Next(1, 101);
+        Maneuverability = random.Next(1, 101);
+    }
+
+    public static bool operator*(Tank firstTank, Tank secondTank)
+    {
+        int firstReception = 0;
+        int secodReception = 0;
+        
+        firstReception += firstTank.Ammunition - secondTank.Ammunition;
+        secodReception += secondTank.Ammunition - firstTank.Ammunition;
+        firstReception += firstTank.LevelOfArmor - secondTank.LevelOfArmor;
+        secodReception += secondTank.LevelOfArmor - firstTank.LevelOfArmor;
+        firstReception += firstTank.Maneuverability - secondTank.Maneuverability;
+        secodReception += secondTank.Maneuverability - firstTank.Maneuverability;
+
+        if (firstReception > secodReception)
+        {
+            return true;
+        }
+        else if (secodReception > firstReception)
+        {
+            return false;
+        }
+        else
+        {
+            throw new ArgumentException("Пат");
+        }
+        
+        
+        
+    }
+
+    public void GetInfo()
+    {
+        Console.WriteLine("------------ {0} ------------", LevelOfArmor);
+        Console.WriteLine("------------ {0} ------------", Ammunition);
+        Console.WriteLine("------------ {0} ------------", Maneuverability);
+    }
     
+}
+
+public class T34 : Tank
+{
+    public string Name = "T34";
+}
+
+public class Pantera : Tank
+{
+    public string Name = "Pantera";
 }
--- TankWar/Program.cs
+++ TankWar/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using MyClassLib.WorldOfTanks;
+
 Console.WriteLine("Hello, World!");
+
+Tank[] battle = new Tank[5];
+for (int i = 0; i < 5; i++)
+{
+    Tank n = new Pantera();
+    Tank c = new T34();
+    if (n * c)
+    {
+        Console.WriteLine(n.GetType() + " победил");
+    }
+    else
+    {
+        Console.WriteLine(c.GetType() + " победил");
+    }
+}
